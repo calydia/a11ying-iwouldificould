4c14a2850cddb88411f86e38b140586702e5c812
Sanna Kramsi <sanna.kramsi@gmail.com>
2026-02-20 12:53:53 +0200

    fix(Redirects): Add missing redirects for newly changed urls

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,6 +21,42 @@ export default defineConfig({
     "/en/fundamentals/the-basics/who-accessibility-is-for" : {
       status: 302,
       destination: "/en/fundamentals/the-basics/who-is-accessibility-for"
+    },
+    "/fi/perusteet/vammaisuuden-tyypit/nakovammaisuus" : {
+      status: 302,
+      destination: "/fi/perusteet/vammaisuuden-tyypit/nakemiseen-liittyvat-rajoitteet"
+    },
+    "/fi/perusteet/vammaisuuden-tyypit/kuulohairiot" : {
+      status: 302,
+      destination: "/fi/perusteet/vammaisuuden-tyypit/kuulemiseen-liittyvat-rajoitteet"
+    },
+    "/fi/perusteet/vammaisuuden-tyypit/puhevammaisuus" : {
+      status: 302,
+      destination: "/fi/perusteet/vammaisuuden-tyypit/puheeseen-liittyvat-rajoitteet"
+    },
+    "/fi/perusteet/vammaisuuden-tyypit/liikkuvuus-joustavuus-ja-ruumiinrakenteeseen-liittyvat-vammat" : {
+      status: 302,
+      destination: "/fi/perusteet/vammaisuuden-tyypit/liikkumiseen-ja-fyysiseen-toimintakykyyn-liittyvat-rajoitteet"
+    },
+    "/fi/perusteet/vammaisuuden-tyypit/kognitiiviset-hairiot" : {
+      status: 302,
+      destination: "/fi/perusteet/vammaisuuden-tyypit/kognitiiviset-ja-oppimiseen-liittyvat-rajoitteet"
+    },
+    "/fi/perusteet/vammaisuuden-tyypit/psykologiset-psyykkiset-vammat" : {
+      status: 302,
+      destination: "/fi/perusteet/vammaisuuden-tyypit/mielenterveyteen-liittyvat-rajoittee"
+    },
+    "/fi/perusteet/vammaisuuden-tyypit/kohtausvammaisuus" : {
+      status: 302,
+      destination: "/fi/perusteet/vammaisuuden-tyypit/valoherkkyys-ja-kohtaukset"
+    },
+    "/fi/perusteet/vammaisuuden-tyypit/monivammaisuus-yhdistelmavammaisuus" : {
+      status: 302,
+      destination: "/fi/perusteet/vammaisuuden-tyypit/monivammaisuus-ja-yhdistelmat"
+    },
+    "/en/fundamentals/theoretical-models-of-disability/economical-model" : {
+      status: 302,
+      destination: "/en/fundamentals/theoretical-models-of-disability/economic-model"
     }
   },
   integrations: [sitemap({
